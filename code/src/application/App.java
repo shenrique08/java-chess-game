baf24a4a372802b2431c8af0880cb8c0f21bf5d3
Sergio Henrique <sergimmatos@hotmail.com>
2025-05-18 09:33:59 -0300

    Creating Position Class

---
+++ code/src/application/App.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardgame.Position;
+
+public class App {
+    public static void main(String[] args) {
+
+        Position startPosition = new Position(0, 0);
+
+        System.out.println(startPosition);
+
+    }
+}
