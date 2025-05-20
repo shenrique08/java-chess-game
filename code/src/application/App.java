956cb7c91609b44190df544d5cb642f1fa8a0f92
Sergio Henrique <sergimmatos@hotmail.com>
2025-05-20 11:53:42 -0300

    handling exceptions

--- code/src/application/App.java
+++ code/src/application/App.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
-
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
@@ -13,17 +15,22 @@ public class App {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(input);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(input);
 
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(input);
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(input);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-            System.out.println();
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch (ChessException | InputMismatchException e) {
+                System.out.println(e.getMessage());
+                input.nextLine();
+            }
         }
-
     }
 }
